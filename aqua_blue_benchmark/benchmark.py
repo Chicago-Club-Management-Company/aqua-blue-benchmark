8e90c659718c45834b99276bbfe4336b5764aaca
lonelyneutrin0 <belagal1@msu.edu>
2025-03-14 18:34:49 -0400

    Added elapsed_time param to result

--- aqua_blue_benchmark/benchmark.py
+++ aqua_blue_benchmark/benchmark.py
@@ -1,14 +1,21 @@
 from dataclasses import dataclass
+from enum import Enum
 
 from typing import Callable, Union
 import numpy as np
 
 from tabulate import tabulate
 
+class Stat(Enum): 
+    MSE = 'mse'
+    RMSE = 'rmse'
+    NRMSE_MEAN = 'nrmse_mean'
+    NRMSE_MINMAX = 'nrmse_minmax'
+
 @dataclass
 class Result: 
     """
-    A container object for the results of a time series prediction 
+    A container object for the results of a timeseries prediction 
     """
 
     model_name: str
@@ -20,10 +27,10 @@ class Result:
     """
     times: The independent variable of the timeseries 
     """
-
-    prediction: np.typing.NDArray[np.floating]
+    
+    dep_var: np.typing.NDArray[np.floating]
     """ 
-    The prediction made by the reservoir
+    The dependent variable predicted by the reservoir
     """
     
     mse: float 
@@ -46,15 +53,25 @@ class Result:
     Normalized Root Mean Square error of a prediction 
     """
 
+    elapsed_time: float
+    """
+    Time taken for the prediction 
+    """
+    
+    def __getattribute__(self, name):
+        if isinstance(name, Stat):
+            return super().__getattribute__(name.value)
+        
+        return super().__getattribute__(name)
 
 
 def compare_results(bench1: Result, bench2: Result):
     # Compare the results of two benchmarks 
-    titles = ["\033[94m Model \033[0m", "\033[94m MSE \033[0m", "\033[94m RMSE \033[0m", "\033[94m NRMSE_MEAN \033[0m", "\033[94m NRMSE_MINMAX \033[0m"]
+    titles = ["\033[94m Model \033[0m", "\033[94m MSE \033[0m", "\033[94m RMSE \033[0m", "\033[94m NRMSE_MEAN \033[0m", "\033[94m NRMSE_MINMAX \033[0m", "\033[94m ELAPSED TIME (s) \033[0m"]
     
     data1 = [bench1.model_name]
     data2 = [bench2.model_name]
-
+    
     # Compare the stats and color accordingly
     
     # MSE
@@ -98,6 +115,15 @@ def compare_results(bench1: Result, bench2: Result):
         data1.append(f'{bench1.nrmse_minmax:.4f}')
         data2.append(f'{bench2.nrmse_minmax:.4f}')
     
-
+    if bench1.elapsed_time < bench2.elapsed_time: 
+        data1.append(f"\033[92m{bench1.elapsed_time:.4f}\033[0m") 
+        data2.append(f'{bench2.elapsed_time:.4f}')
+    elif bench1.elapsed_time > bench2.elapsed_time:
+        data2.append(f"\033[92m {bench2.elapsed_time:.4f} \033[0m")
+        data1.append(f'{bench1.elapsed_time:.4f}')
+    else: 
+        data1.append(f'{bench1.elapsed_time:.4f}')
+        data2.append(f'{bench2.elapsed_time:.4f}')
+    
     data = [data1, data2]
     print(tabulate(data, headers=titles, tablefmt='grid'))
